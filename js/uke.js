f0739796724d0f200d990f1b8dd9a78cbdd7f035
Nathaniel Richand <nrichand@gmail.com>
2016-09-20 15:53:02 +0200

    login workflow implemented

--- js/uke.js
+++ js/uke.js
@@ -1,4 +1,5 @@
 var songs;
+var loggedUser;
 
 function displaySongsList(query){
 	Helpers.withPrismic(function(ctx) {
@@ -83,4 +84,72 @@ function printDiv(divName) {
      window.print();
 
      document.body.innerHTML = originalContents;
+}
+
+function initFirebase(){
+  var config = {
+    apiKey: "AIzaSyDt379V8lGTwY8B2fXyy1CxIGhVxVgKSDg",
+    authDomain: "ukulele-600b8.firebaseapp.com",
+    databaseURL: "https://ukulele-600b8.firebaseio.com",
+    storageBucket: "ukulele-600b8.appspot.com",
+    messagingSenderId: "201984941780"
+  };
+  firebase.initializeApp(config);
+}
+
+function loginHandler(){
+    $("#login").on('click', function() {
+        if(loggedUser){
+            firebase.auth().signOut().then(function() {              
+              location.reload();
+            }, function(error) {
+              console.log(error);
+            });
+        } else {
+            window.location='login.html';
+        }
+    });    
+}
+
+function initUser(){
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        loggedUser = user;
+
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var uid = user.uid;
+        var providerData = user.providerData;
+        user.getToken().then(function(accessToken) {
+            $("#username").text(displayName);
+            $("#login").html('<i class="fa fa-sign-out" aria-hidden="true"></i>Logout');
+            $("#login").addClass("secondary");
+
+/*
+          document.getElementById('sign-in-status').textContent = 'Signed in';
+          document.getElementById('sign-in').textContent = 'Sign out';
+          document.getElementById('account-details').textContent = JSON.stringify({
+            displayName: displayName,
+            email: email,
+            emailVerified: emailVerified,
+            photoURL: photoURL,
+            uid: uid,
+            accessToken: accessToken,
+            providerData: providerData
+          }, null, '  ');
+*/
+        });
+      } else {
+        loggedUser = null;
+        // User is signed out.
+        //document.getElementById('sign-in-status').textContent = 'Signed out';
+        //document.getElementById('sign-in').textContent = 'Sign in';
+        //document.getElementById('account-details').textContent = 'null';
+      }
+    }, function(error) {
+      console.log(error);
+    });
 }
